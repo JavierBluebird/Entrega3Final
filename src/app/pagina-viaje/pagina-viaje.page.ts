a95e59cc74b242f36408e714e55ce12b6d96583c
Javier Gonzalez <javierignaciogc@gmail.com>
2023-10-30 15:19:06 -0300

    Update pagina-viaje.page.ts

--- src/app/pagina-viaje/pagina-viaje.page.ts
+++ src/app/pagina-viaje/pagina-viaje.page.ts
@@ -34,7 +34,7 @@ export class PaginaViajePage implements OnInit {
             lat: -33.43283572901143,
             lng: -70.61490440499237,
           },
-          zoom: 16,
+          zoom: 16, // zoom total
     },
     })
   }
